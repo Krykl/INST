5bb688f63d251677f1c58a5a879bb2576f838ed6
Krykl <ana.lopez@DUIO814001.mintel.int>
2015-07-14 14:44:58 -0500

    Log. conexiones a web services y conexiones a portal web
    Valores por indicadores e institución y calculo de log. del conexiones a
    web services y conexiones a portal web

---
+++ data_infod_inst_std.sql
@@ -0,0 +1,9 @@
+﻿select	*, 
+	round((log(conx_ws+1) /
+		(select max(log(conx_ws+1)) from "INST".vdata_infod_inst ))::numeric,4) as log_conx_ws,
+
+	round((log(conx_pweb+1) /
+		(select max(log(conx_pweb+1)) from "INST".vdata_infod_inst ))::numeric,4) as log_conx_pweb
+
+ from	"INST".vdata_infod_inst
+
